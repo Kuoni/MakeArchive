1c23d36d41b1d525e17e3e1b7217f75edfe41b9c
ibabinruel <ibabinruel@spiria.com>
2016-02-12 15:18:34 -0500

    Added argparse and added 1 argument

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import argparse
 import os
 
 from MakeArchive import MakeArchive
 from MakeArchive import ReadConfig
 
 
-def execute():
-    reader = ReadConfig("config.txt")
+def execute(conf_file_path):
+    reader = ReadConfig(conf_file_path)
     dir_list = reader.read()
-    print("zipping: {dirn}".format(dirn=dir_list[0]))
+    
     make = MakeArchive()
     make.create_with_dirs(dir_list, os.getcwd(), r"F:\\test")
 
 if __name__ == "__main__":
-    execute()
+    arg_parse = argparse.ArgumentParser(
+        description="Copies folder given in a config list and zips them together finally" +
+                    " it copies the zip to a final destination.")
+    arg_parse.add_argument("-c", "--config", dest="config", default="config.txt",
+                           help="specify this flag if you want to have the config read from somewhere else.")
+
+    the_args = arg_parse.parse_args()
+    execute(the_args.config)
